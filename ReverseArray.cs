9d6cf8025d51b3a7edcb8f3bd0a12b5428310525
PenevRBM <32866178+PenevRBM@users.noreply.github.com>
2017-10-17 14:46:15 +0300

    Add files via upload

---
+++ ReverseArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+            
+                Console.Write(string.Join(" ", arr));
+            
+
+
+
+
+
+        }
+    }
+}
