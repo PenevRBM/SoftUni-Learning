d954452163d3f741a9a64933c18494e7d8091ff1
PenevRBM <32866178+PenevRBM@users.noreply.github.com>
2017-10-17 12:16:31 +0300

    Add files via upload

---
+++ Day of weak.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Dayofweak
+    {
+        static void Main(string[] args)
+        {
+            double[] nums =
+                Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            foreach (var x in nums)
+            {
+                double rounded = Math.Round(x, 0, MidpointRounding.AwayFromZero);
+                Console.WriteLine(x + "=>" + rounded);
+            }
+        }
+
+
+    }
+}
+
---
+++ ReverseArrayofIntegers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Arrays
+{
+    class ReverseArrayofIntegers
+    {
+        var n = int.Parse(Console.ReadLine());
+        var arr = new int[n];
+        for (int i = 0; i<n; i++)
+			{
+             arr[i] = int.Parse(Console.ReadLine);
+			}
+
+
+    }
+}
