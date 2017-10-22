1c3df5498fecee8a5060cb1a1db1c568748e5047
PenevRBM <32866178+PenevRBM@users.noreply.github.com>
2017-10-23 00:22:21 +0300

    Add files via upload

---
+++ DemoList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers =  new List<int>();
+            string inputLine = Console.ReadLine();
+            while (inputLine != "Exit")
+            {
+                int number = int.Parse(inputLine);
+                numbers.Add(number);
+                inputLine = Console.ReadLine();
+
+            }
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}
